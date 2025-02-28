4071e39a3d1f39ed0fc6c2e125319a482a89c782
jhonatanchiari <jhonatanchiari@gmail.com>
2025-02-28 20:12:11 -0300

    Enviando arquivo Hola

---
+++ Hola.java
@@ -0,0 +1,13 @@
+public class Hola {
+    public static void main(String[] args) {
+        holaQueTal();
+    }
+
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
+    }
+}
